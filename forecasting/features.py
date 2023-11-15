4fdfa87d37358b44d35fe04448b6acc0c9de70aa
AguilarAngel9 <anpel.aguiar@gmail.com>
2023-11-14 22:02:05 -0600

    Basic features added

---
+++ forecasting/features.py
@@ -0,0 +1,42 @@
+# Features library.
+# Author: @THEFFTKID
+
+import pandas as pd
+import numpy as np
+
+
+def n_rolling_mean(
+    x_: pd.Series, n: int
+) -> pd.Series:
+    """
+    Calculated the N day rolling mean.
+    """
+    return x_.rolling(window=n).mean().fillna(0)
+
+
+def weighted_n_moving_avg(
+    x_: pd.Series, n: int
+) -> pd.Series:
+    """
+    Calculates the N day weighted rolling mean.
+    """
+    weights = np.arange(1, n + 1)
+    wma = x_.rolling(window=n).apply(
+        lambda x: np.dot(x, weights) / weights.sum(),
+        raw=True
+    ).fillna(0)
+    return wma
+
+
+def momentum(x_: pd.Series, n: int) -> pd.Series:
+    """
+    Calculates the n day momentum.
+    """
+    hare = np.arange(n, len(x_))
+    # Momentum.
+    m = [0] * n
+    for i in hare:
+        m.append(
+            x_[i] - x_[i - n]
+        )
+    return pd.Series(m)
