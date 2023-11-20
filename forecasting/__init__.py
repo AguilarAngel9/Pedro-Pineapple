612722a54d8e672cc6d30fdc9a905b4d7f43ee27
Camila Cusicanqui <camilacusicanqui@gmail.com>
2023-11-20 00:39:06 -0600

    Added evaluation metrics and labels

--- forecasting/__init__.py
+++ forecasting/__init__.py
@@ -9,11 +9,19 @@ from .dynamic_threshold import (
 from .environments import (
     Forecasting
 )
+from .evaluation import (
+    create_labels,
+    data_splitter,
+    evaluation_metrics
+)
 
 # Keep alphabetical order.
 __all__ = [
     # Functions.
+    "create_labels"
     "define_threshold",
+    "data_splitter",
+    "evaluation_metrics"
     "momentum",
     "n_rolling_mean",
     "weighted_n_moving_avg",
