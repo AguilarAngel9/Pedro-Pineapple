690786d30aeb0671b02b630492d1301b61062cba
Camila Cusicanqui <camilacusicanqui@gmail.com>
2023-11-19 22:12:12 -0600

    Threshold added to features.py
    holiiii cami

--- forecasting/features.py
+++ forecasting/features.py
@@ -3,7 +3,8 @@
 
 import pandas as pd
 import numpy as np
-
+from scipy.stats import t
+import matplotlib.pyplot as plt
 
 def n_rolling_mean(
     x_: pd.Series, n: int
@@ -28,7 +29,10 @@ def weighted_n_moving_avg(
     return wma
 
 
-def momentum(x_: pd.Series, n: int) -> pd.Series:
+def momentum(
+        x_: pd.Series,
+        n: int
+) -> pd.Series:
     """
     Calculates the n day momentum.
     """
@@ -40,3 +44,57 @@ def momentum(x_: pd.Series, n: int) -> pd.Series:
             x_[i] - x_[i - n]
         )
     return pd.Series(m)
+
+def plot_tdist(
+        dof: float,
+        loc: float,
+        scale: float,
+        data:pd.Series
+):
+    x = np.linspace(min(data), max(data), 1000)
+    y = t.pdf(x, dof, loc, scale)
+    plt.hist(data, bins=30, density=True, alpha=0.5, label='Data Histogram')
+    plt.plot(x, y, label='Fitted t-Distribution')
+    plt.xlabel('Value')
+    plt.ylabel('Density')
+    plt.title('t-Distribution Fit to Data')
+    plt.legend()
+    plt.show()
+
+
+def define_threshold(
+    df:pd.Series,
+    lower_bound: float,
+    upper_bound: float,
+    override_plot = False        
+)-> tuple:
+    
+    '''
+    This function fits a t-distribution to a pandas Series (df) and calculates upper
+    and lower thresholds based on specified percentiles (upper_bound and lower_bound).
+    It utilizes the t-distribution's percent point function (ppf) to determine these 
+    thresholds, effectively identifying the range of values corresponding to the given 
+    percentiles in the data's distribution
+
+    Parameters: 
+
+    - df (pd.Series): The data series to which the t-distribution is to be fitted.
+    - lower_bound (float): The lower percentile for calculating the lower threshold. 
+      Value should be between 0 and 1.
+    - upper_bound (float): The upper percentile for calculating the upper threshold. 
+      Value should be between 0 and 1.
+    - override_plot (bool): If set to True, a plot of the t-distribution is generated.
+    
+    Returns:
+    - tuple: A tuple containing the calculated upper and lower thresholds.
+    '''
+
+    dof, loc, scale = t.fit(df)
+
+    if override_plot == True:
+        plot_tdist(dof,loc,scale,df)
+
+    threshold_upper = t.ppf(upper_bound, dof, loc, scale)
+    threshold_lower = t.ppf(lower_bound, dof, loc, scale)
+
+    return (threshold_upper, threshold_lower)
